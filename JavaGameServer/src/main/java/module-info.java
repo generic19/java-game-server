25df1a7b438c869571254fe89fc3896ad52b7b29
Basil Alasadi <8195420+generic19@users.noreply.github.com>
2025-01-14 23:43:40 +0200

    Added MySQL connector dependency.

--- JavaGameServer/src/main/java/module-info.java
+++ JavaGameServer/src/main/java/module-info.java
@@ -1,7 +1,9 @@
 module com.mycompany.javagameserver {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires java.sql;
+    requires mysql.connector.j;
+    
     opens com.mycompany.javagameserver to javafx.fxml;
     exports com.mycompany.javagameserver;
 }
