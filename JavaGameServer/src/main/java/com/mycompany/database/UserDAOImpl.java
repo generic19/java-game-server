98646b7c553312fe313d8d28c5c6a2df6921f3fb
arwakhaled24 <110431648+arwakhaled24@users.noreply.github.com>
2025-01-15 09:29:28 +0200

    login in UserDAO

--- JavaGameServer/src/main/java/com/mycompany/database/UserDAOImpl.java
+++ JavaGameServer/src/main/java/com/mycompany/database/UserDAOImpl.java
@@ -2,6 +2,7 @@ package com.mycompany.database;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class UserDAOImpl implements UserDAO {
 
@@ -33,8 +34,31 @@ public class UserDAOImpl implements UserDAO {
     }
 
     @Override
-    public void login(UserDTO user) {
-      
+   public void login(UserDTO user) {
+        String query = "SELECT password FROM users WHERE username = ?";
+        try (Connection connection =Database.getInstance().getConnection()){
+             PreparedStatement preparedStatement = connection.prepareStatement(query) ;
+
+       
+            preparedStatement.setString(1, user.getUsername());
+
+            
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                String storedHashedPassword = resultSet.getString("password");
+
+               
+                if (user.getPasswordHash().equals(storedHashedPassword)) {
+                    System.out.println("Login successful. Welcome, " + user.getUsername() + "!");
+                } else {
+                    System.out.println("Invalid username or password.");
+                }
+            } else {
+                System.out.println("User not found.");
+            }
+        } catch (Exception e) {
+                e.printStackTrace();
+        }
     }
 
     @Override
