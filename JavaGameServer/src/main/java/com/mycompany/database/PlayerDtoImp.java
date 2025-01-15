94ea64340f7c41b34f6720c9fabf3056eaf8c372
arwakhaled24 <110431648+arwakhaled24@users.noreply.github.com>
2025-01-15 23:12:21 +0200

    player DTO implementation

---
+++ JavaGameServer/src/main/java/com/mycompany/database/PlayerDtoImp.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.database;
+
+/**
+ *
+ * @author ArwaKhaled
+ */
+public class PlayerDtoImp implements PlayerDTO {
+    String userName ;
+    int score;
+    boolean isAvailable ;
+
+    public PlayerDtoImp(String userName, int score, boolean isAvailable) {
+        this.userName = userName;
+        this.score = score;
+        this.isAvailable = isAvailable;
+    }
+
+    @Override
+    public String getUsername() {
+        return userName;
+    }
+
+    @Override
+    public int getScore() {   
+        return score;
+    }
+
+    @Override
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+    
+}
