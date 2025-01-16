da3dabbdaee0bc40c0790b951f6ac734d52b7c7c
Hassan Ali <hassanaliielsayed@gmail.com>
2025-01-17 01:19:34 +0200

    creating invite request and response classes

---
+++ JavaGameServer/src/main/java/com/mycompany/networking/matching/InviteRequest.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.networking.matching;
+
+/**
+ *
+ * @author AhmedAli
+ */
+public class InviteRequest implements MatchingMessage{
+    private String userName;
+
+    public InviteRequest(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+    
+    
+}
