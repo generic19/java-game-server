0b7c1051272523804b284911d9255f765c70c5d4
Hassan Ali <hassanaliielsayed@gmail.com>
2025-01-17 00:18:29 +0200

    creating network interfaces

---
+++ JavaGameServer/src/main/java/com/mycompany/networking/authentication/AuthMessage.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.mycompany.networking.authentication;
+
+import com.mycompany.networking.Message;
+
+/**
+ *
+ * @author AhmedAli
+ */
+public interface AuthMessage extends Message {
+    
+}
---
+++ JavaGameServer/src/main/java/com/mycompany/networking/matching/MatchingMessage.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.mycompany.networking.matching;
+
+import com.mycompany.networking.Message;
+/**
+ *
+ * @author AhmedAli
+ */
+public interface MatchingMessage extends Message {
+    
+}
