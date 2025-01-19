44f81b672680f5616cd8302b5a4be91a8b4dc214
Basil Alasadi <8195420+generic19@users.noreply.github.com>
2025-01-19 14:56:04 +0200

    Added BaseHandler and Request

---
+++ JavaGameServer/src/main/java/com/mycompany/javagameserver/handling/BaseHandler.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.javagameserver.handling;
+
+import com.mycompany.networking.Communicator;
+import com.mycompany.networking.Message;
+
+/**
+ *
+ * @author basel
+ */
+public class BaseHandler {
+    private Communicator communicator;
+    private BaseHandler next;
+
+    BaseHandler(Communicator communicator) {
+        this.communicator = communicator;
+    }
+
+    public BaseHandler getNext() {
+        return next;
+    }
+    
+    public void handle(Request request) {
+        if (next != null) {
+            this.next.handle(request);
+        }
+    }
+    
+    protected void sendMessage(Message message) {
+        communicator.sendMessage(message);
+    }
+}
