73d8556afd6a9fc4c2c56a84cf7adcc44c32388b
Basil Alasadi <8195420+generic19@users.noreply.github.com>
2025-01-19 15:25:23 +0200

    Create OnlinePlayer.java

---
+++ JavaGameServer/src/main/java/com/mycompany/networking/OnlinePlayer.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.networking;
+
+/**
+ *
+ * @author basel
+ */
+public class OnlinePlayer {
+    private final String username;
+    private final int score;
+
+    public OnlinePlayer(String username, int score) {
+        this.username = username;
+        this.score = score;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public int getScore() {
+        return score;
+    }
+}
